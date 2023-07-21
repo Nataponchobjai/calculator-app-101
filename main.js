9d47fedf468004521301ced59a688bd00fdf9407
Nataponchobjai <nataponchobjai@gmail.com>
2023-07-21 01:00:06 -0500

    aster

--- main.js
+++ main.js
@@ -32,13 +32,22 @@ const subtract = (numA, numB) => {
 const multiply = (numA, numB) => {
   // * to get a product then return it
   // Open up the inspector tool in Chrome and select the Console tab to see what this functions is "logging out" to the console.
-  console.log(numA, numB)
+  const product = numA * numB
+  return product
 }
 
-const divide = null
+const divide = (numA, numB) => {
+  const quotient = numA / numB
+  return quotient
+}
+
 // / to get a quotient,
 
-const modulus = null
+const modulus = (numA, numB) => {
+  const remainder = numA % numB
+  return remainder
+}
+
 // and % to get a remainder.
 
 // This function changes the "operation" variable to be equal to the "id" of the button we choose on the web page.
@@ -58,19 +67,23 @@ const putResultInElement = (operationResults) => {
 }
 
 // The function uses the value of "operation" variable to determine which operation function it should use on the number: add, subtract, multiply, divide, or modulus
+
 const equals = () => {
   switch (operation) {
-    case "addition":  putResultInElement(add(firstNum, secondNum)) 
-    break;
-    case "subtraction": putResultInElement(subtract(firstNum, secondNum)) 
-    break;
-    case "multiplication": multiply(firstNum, secondNum) 
-    break;
-    case "division": console.log(divide(firstNum, secondNum)) 
-    break;
-    case "modulus": console.log(modulus(firstNum, secondNum)) 
-    break;
-    default: "Choose an operation"
+    case "addition":
+      putResultInElement(add(firstNum, secondNum));
+      break;
+    case "subtraction":
+      putResultInElement(subtract(firstNum, secondNum));
+      break;
+    case "multiplication":
+      putResultInElement(multiply(firstNum, secondNum));
+      break;
+    case "division":
+      putResultInElement(divide(firstNum, secondNum));
+      break;
+    case "modulus":
+      putResultInElement(modulus(firstNum, secondNum));
+      break;
   }
 }
-
